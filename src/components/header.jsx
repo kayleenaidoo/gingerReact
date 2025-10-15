77237b0e803e0e56a1bf6fc6b63e2917c296a4a3
kayleenaidoo <26215861@sun.ac.za>
2025-10-15 00:33:24 +0000

    themes and header buttons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,25 +4,25 @@ import Icon from "@mdi/react";
 
 export default function Header() {
     return (
-        <header className="secondary flex w-full items-center justify-between bg-rose-50 px-6 py-4">
+        <header className="secondary flex w-full items-center justify-between bg-rose-50 px-6 py-2">
             <div>
-                <img src={logo} alt="Ink & Ginger Logo" className="h-24" />
+                <img src={logo} alt="Ink & Ginger Logo" className="h-28" />
             </div>
 
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-6">
                 <div className="flex gap-4">{/* if (!loggedIn || guest) */}
-                    <div>
-                        sign in
-                    </div>
-                    <div>
-                        register
-                    </div>
+                    <button className="button-tertiary py-2 px-4">
+                        Sign in
+                    </button>
+                    <button className="button-secondary py-2 px-4">
+                        Register
+                    </button>
                 </div>
                 <div className="text-xl font-medium primary"> {/* else*/}
-                    username
+                    Hi, @username
                 </div>
-                <Icon path={mdiBellOutline} size={2} className="icon" />
-                <Icon path={mdiCogOutline} size={2} className="icon" />
+                <Icon path={mdiBellOutline} size={1.8} className="primary" />
+                <Icon path={mdiCogOutline} size={1.8} className="primary" />
             </div>
         </header>
     );
